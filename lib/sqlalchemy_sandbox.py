7eeff86fc401e9d6f4c93a60b86b1fe92dd7609f
Valerie <valeriejeptoo4@gmail.com>
2023-09-04 14:40:29 +0300

    Done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker  # Import sessionmaker
+from sqlalchemy.orm import sessionmaker  
 
 Base = declarative_base()
 
